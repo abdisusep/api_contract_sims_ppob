cc179209fb0d2c730b47643e03078987f9ad5a45
Abdi Susep <susepsupriatna@gmail.com>
2024-11-25 10:08:08 +0700

    Update transaction.controller.js

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,17 +1,55 @@
+const service = require('../services/transaction.service');
+
 const getBalance = async (req, res) => {
-   res.send('get balance');
+   try {
+      const result = await service.getBalance(req.email);
+      res.status(200).json({
+         status: 0,
+         message: '"Get Balance Berhasil',
+         data: result
+      });
+   } catch (err) {
+      res.status(500).json({ error: err.message });
+   }
 }
 
 const insertTopup = async (req, res) => {
-   res.send('insert topup');
+   try {
+      const result = await service.insertTopup(req.email, req.body);
+      res.status(200).json({
+         status: 0,
+         message: 'Top Up Balance berhasil',
+         data: result
+      });
+   } catch (err) {
+      res.status(500).json({ error: err.message });
+   }
 }
 
 const insertTransaction = async (req, res) => {
-   res.send('insert transaction');
+   try {
+      const result = await service.insertTransaction(req.email, req.body);
+      res.status(200).json({
+         status: 0,
+         message: 'Transaksi berhasil',
+         data: result
+      });
+   } catch (err) {
+      res.status(500).json({ error: err.message });
+   }
 }
 
 const getTransactionHistory = async (req, res) => {
-   res.send('get transaction history');
+   try {
+      const result = await service.getTransactionHistory(req.email);
+      res.status(200).json({
+         status: 0,
+         message: 'Get History Berhasil',
+         data: result
+      });
+   } catch (err) {
+      res.status(500).json({ error: err.message });
+   }
 }
 
 module.exports = { getBalance, insertTopup, insertTransaction, getTransactionHistory }
