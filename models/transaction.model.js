2897d64d3f6c89dd8b042affbabccb250601fe47
Abdi Susep <susepsupriatna@gmail.com>
2024-11-25 10:31:18 +0700

    Update transaction.model.js

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,35 +1,35 @@
 const db = require('../config/db.config');
 
 const getBalance = async (email) => {
-   const [rows] = await db.query('SELECT 100000 AS balanceers');
-   return rows[0];
+   // const [rows] = await db.query('SELECT 100000 AS balanceers');
+   return 1;
 }
 
 const insertTopup = async (email, data) => {
-    const { email, first_name, last_name, password } = data;
+    // const { email, first_name, last_name, password } = data;
 
-    const result = await db.query(
-        'INSERT INTO users (email, first_name, last_name, password) VALUES (?, ?, ?, ?)',
-        [email, first_name, last_name, password]
-    );
+    // const result = await db.query(
+    //     'INSERT INTO users (email, first_name, last_name, password) VALUES (?, ?, ?, ?)',
+    //     [email, first_name, last_name, password]
+    // );
 
-    return result;
+    return 1;
 }
 
 const insertTransaction = async (email, data) => {
-    const { email, first_name, last_name, password } = data;
+    // const { email, first_name, last_name, password } = data;
 
-    const result = await db.query(
-        'INSERT INTO users (email, first_name, last_name, password) VALUES (?, ?, ?, ?)',
-        [email, first_name, last_name, password]
-    );
+    // const result = await db.query(
+    //     'INSERT INTO users (email, first_name, last_name, password) VALUES (?, ?, ?, ?)',
+    //     [email, first_name, last_name, password]
+    // );
 
-    return result;
+    return 1;
 }
 
 const getTransactionHistory = async (email) => {
-   const [rows] = await db.query('SELECT service_code, service_name, service_icon, service_tarif FROM services');
-   return rows;
+   // const [rows] = await db.query('SELECT service_code, service_name, service_icon, service_tarif FROM services');
+   return 1;
 }
 
 module.exports = {
