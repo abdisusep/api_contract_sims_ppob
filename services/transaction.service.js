a9d3db80055aa6fb010df0505833eda31ee24647
Abdi Susep <susepsupriatna@gmail.com>
2024-11-25 10:24:49 +0700

    Update transaction.service.js

--- services/transaction.service.js
+++ services/transaction.service.js
@@ -1,19 +1,23 @@
 const model = require('../models/transaction.model');
 
 const getBalance = async (email) => {
-    return await model.getBalance(email);
+    // return await model.getBalance(email);
+    return { balance: 100000 };
 }
 
 const insertTopup = async (email, data) => {
-    return await model.insertTopup(email, data);
+    // return await model.insertTopup(email, data);
+    return 1;
 }
 
 const insertTransaction = async (email, data) => {
-    return await model.insertTransaction(email, data);
+    // return await model.insertTransaction(email, data);
+    return 1;
 }
 
 const getTransactionHistory = async (email) => {
-    return await model.getTransactionHistory(email);
+    // return await model.getTransactionHistory(email);
+    return 1;
 } 
 
 module.exports = { 
