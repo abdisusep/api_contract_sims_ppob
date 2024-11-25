f6feef0f269a602d8afef21b973da9e8530ad982
Abdi Susep <susepsupriatna@gmail.com>
2024-11-25 16:43:55 +0700

    Update transaction.model.js

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -11,14 +11,17 @@ const getBalance = async (email) => {
 const insertTopup = async (email, data) => {
    const user = await getUserByEmail(email);
    const user_id = user.id;
-    // const { email, first_name, last_name, password } = data;
+   
+   const invoice_number = await getInvoice(user_id);
+   const description = 'Top Up Balance';
+   const { top_up_amount } = data;
 
-    // const result = await db.query(
-    //     'INSERT INTO users (email, first_name, last_name, password) VALUES (?, ?, ?, ?)',
-    //     [email, first_name, last_name, password]
-    // );
+    const result = await db.query(
+        'INSERT INTO transactions (user_id, invoice_number, transaction_type, description, total_amount, created_on) VALUES (?, ?, ?, ?, ?, NOW())',
+        [user_id, invoice_number, description, top_up_amount]
+    );
 
-    return 1;
+    return result;
 }
 
 const insertTransaction = async (email, data) => {
@@ -45,6 +48,16 @@ const getTransactionHistory = async (email) => {
 const getUserByEmail = async (email) => {
     const [rows] = await db.query('SELECT * FROM users WHERE email=?', [email]);
     return rows[0];
+}
+
+const getServiceById = async (id) => {
+    const [rows] = await db.query('SELECT * FROM services WHERE id=?', [id]);
+    return rows[0];
+}
+
+const getInvoice = async (user_id) => {
+    const [rows] = await db.query('SELECT COUNT(*) AS count FROM transactions WHERE user_id=?', [user_id]);
+    return 'INV-' + rows[0].count;
 }
 
 module.exports = {
