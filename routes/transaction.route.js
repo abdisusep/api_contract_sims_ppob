9a073452a1a48660e02094da015b9d295c2a4a1f
Abdi Susep <susepsupriatna@gmail.com>
2024-11-25 09:56:55 +0700

    Update transaction.route.js

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const controller = require('../controllers/transaction.controller');
 const authenticateToken = require('../middleware/auth.middleware');
 
-router.get('/balance', controller.getBalance);
-router.post('/topup', controller.insertTopup);
-router.post('/transaction', controller.insertTransaction);
-router.get('/transaction/history', controller.getTransactionHistory);
+router.get('/balance', authenticateToken, controller.getBalance);
+router.post('/topup', authenticateToken, controller.insertTopup);
+router.post('/transaction', authenticateToken, controller.insertTransaction);
+router.get('/transaction/history', authenticateToken, controller.getTransactionHistory);
 
 module.exports = router;
